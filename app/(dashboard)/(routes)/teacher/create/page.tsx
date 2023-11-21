91431a0c44cba699445677b8ba9282637853e87c
sankrkall2 <gowrisankarkalla4@gmail.com>
2023-11-21 09:47:54 +0530

    changed

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -53,7 +53,7 @@ const Create = () => {
       <div>
         <h1 className="text-2xl">Name Your course</h1>
         <p className="text-sm text-slate-600">
-          What would you like to name your course? Don't Worry, You can this
+          What would you like to name your course? Dont Worry, You can this
           change later
         </p>
         <Form {...form}>
